d98fa1e59ada805681d0e5193e2bb26dcf1fac23
PaneruVipin <paneruvipinbusiness@gmail.com>
2025-05-09 01:18:50 +0530

    miner

--- backend/tracker/filters.py
+++ backend/tracker/filters.py
@@ -4,8 +4,8 @@ from .models import Transaction
 class TransactionFilter(django_filters.FilterSet):
     amount_min = django_filters.NumberFilter(field_name="amount", lookup_expr="gte")
     amount_max = django_filters.NumberFilter(field_name="amount", lookup_expr="lte")
-    date_from = django_filters.DateFilter(field_name="date", lookup_expr="gte")
-    date_to = django_filters.DateFilter(field_name="date", lookup_expr="lte")
+    date_from = django_filters.DateFilter(field_name="created_at", lookup_expr="gte")
+    date_to = django_filters.DateFilter(field_name="created_at", lookup_expr="lte")
     category = django_filters.NumberFilter(field_name="category_id")
 
     class Meta:
