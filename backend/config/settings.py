d8b7b1b21a37b762331e67fd7e7ffac0bd1e2f44
PaneruVipin <paneruvipinbusiness@gmail.com>
2025-05-09 05:54:34 +0530

    miner

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -151,9 +151,9 @@ REST_FRAMEWORK = {
     # 'DEFAULT_RENDERER_CLASSES': [
     #     'rest_framework.renderers.JSONRenderer',  
     # ],
-    # 'DEFAULT_PARSER_CLASSES': [
-    #     'rest_framework.parsers.JSONParser',
-    # ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
